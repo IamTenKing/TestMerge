31c00846e170ddde23bd8abf42b2ab491cac1ffd
jinting <iamten@aliyun.com>
2020-10-24 20:10:17 +0800

    test

---
+++ test/src/main/java/com/test/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.test.controller;
+
+/**
+ * Date:2020/10/24
+ */
+public class TestController {
+
+    public static void main(String[] args) {
+        System.out.println("testetstsetstsetetsetest############");
+
+
+
+
+    }
+}
